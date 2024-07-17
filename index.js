42de59dd50be7ef2ca51c3185f56315516a27482
Israel <israeldianga@gmail.com>
2024-07-17 13:45:39 +0300

    New

--- index.js
+++ index.js
@@ -80,10 +80,11 @@ app.post("/add", async (req, res) => {
 
 app.post("/user", async (req, res) => {
   if (req.body.add === "new") {
+    
     res.render("new.ejs");
   } else {
     currentUserId = req.body.user;
-    
+
     res.redirect("/");
   }
 });
