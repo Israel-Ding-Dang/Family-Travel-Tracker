4b23f61d2b8a3dfad16d9ef55cef5c2561973467
Israel <israeldianga@gmail.com>
2024-07-17 13:47:16 +0300

    else

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ app.post("/add", async (req, res) => {
 
 app.post("/user", async (req, res) => {
   if (req.body.add === "new") {
-    
     res.render("new.ejs");
-  } else {
+    
+  } else {    
     currentUserId = req.body.user;
 
     res.redirect("/");
