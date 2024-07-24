848872e49a513d3dc47e00fb4f3437f3904d48df
Israel <israeldianga@gmail.com>
2024-07-24 13:23:02 +0300

    Safe

--- index.js
+++ index.js
@@ -79,11 +79,12 @@ app.post("/add", async (req, res) => {
 });
 
 app.post("/user", async (req, res) => {
-  
+
   if (req.body.add === "new") {
     res.render("new.ejs");  
 
-  } else {  
+  } else { 
+     
     currentUserId = req.body.user;
 
     res.redirect("/");
