9794913e3f30e54983ff3de224fc344df24c94ff
Israel <israeldianga@gmail.com>
2024-07-31 13:10:21 +0300

    Lowercase

--- index.js
+++ index.js
@@ -64,13 +64,12 @@ app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
   const currentUser = await getCurrentUser();
-
   try {
 
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]
-      
+
     );
 
     const data = result.rows[0];
