2808537c34b155c3f38381243d53f32787d33ad2
Israel <israeldianga@gmail.com>
2024-08-05 12:50:19 +0300

    Response

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/add", async (req, res) => {
-  
+
   const input = req.body["country"];
   const currentUser = await getCurrentUser();
 
@@ -79,7 +79,6 @@ app.post("/add", async (req, res) => {
       );
 
       res.redirect("/");
-
     } catch (err) {
       console.log(err);
     }
