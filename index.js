7726760271a47766c922829c07a9a6316dbd7688
Israel <israeldianga@gmail.com>
2024-08-19 13:40:04 +0300

    Console

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
   //const currentUser = await getCurrentUser();
-  
+
   try {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
@@ -75,7 +75,6 @@ app.post("/add", async (req, res) => {
         [countryCode, currentUserId]
       );
       res.redirect("/");
-
     } catch (err) {
       console.log(err);      
     }
