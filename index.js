e17ea4b22ad7ebad83b307c4424dd9839f509357
Israel <israeldianga@gmail.com>
2024-08-21 13:24:22 +0300

    Body

--- index.js
+++ index.js
@@ -52,8 +52,7 @@ app.get("/", async (req, res) => {
     total: countries.length,
     users: users,
     color: currentUser.color,
-  });
-  
+  });  
 });
 
 app.post("/add", async (req, res) => {
