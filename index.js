0bcc9800d3f19b7275404f4805ee978b98c518c6
Israel <israeldianga@gmail.com>
2024-08-26 13:08:29 +0300

    If

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post("/add", async (req, res) => {
     );
     const data = result.rows[0];
     const countryCode = data.country_code;
-    
+
     try {            
       await db.query(
         "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
@@ -82,7 +82,6 @@ app.post("/add", async (req, res) => {
 });
 
 app.post("/user", async (req, res) => {
-
   if (req.body.add === "new") {
     res.render("new.ejs");  
   } else {      
