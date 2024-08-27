523f7fdc59563bb08a23dd2485538b470ef510b0
Israel <israeldianga@gmail.com>
2024-08-27 13:10:31 +0300

    Select

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ async function checkVisited() {
 
 async function getCurrentUser() {
   const result = await db.query("SELECT * FROM users");
+  
   users = result.rows; 
 
   return users.find((user) => user.id == currentUserId);
