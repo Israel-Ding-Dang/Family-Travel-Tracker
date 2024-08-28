2f06d4bede0b9a25f9964e5c80ebe36dc372090e
Israel <israeldianga@gmail.com>
2024-08-28 13:18:40 +0300

    Err

--- index.js
+++ index.js
@@ -74,12 +74,13 @@ app.post("/add", async (req, res) => {
       );
 
       res.redirect("/");
-      
+
     } catch (err) {
       console.log(err);      
     }
   } catch (err) {    
     console.log(err);
+    
   }
 });
 
