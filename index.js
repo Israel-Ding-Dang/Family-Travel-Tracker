a3b1db35972745a4b881d4fbf19a7ca2b274a40e
Israel <israeldianga@gmail.com>
2024-09-24 13:33:38 +0300

    Countries

--- index.js
+++ index.js
@@ -34,11 +34,10 @@ async function checkVisited() {
   result.rows.forEach((country) => {    
 
     countries.push(country.country_code);   
-            
+
   });  
 
   return countries; 
-
 }
 
 async function getCurrentUser() {
