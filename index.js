fc749bf436be3e4d71ba01696b42727336decb0d
Israel <israeldianga@gmail.com>
2024-09-27 13:14:41 +0300

    Minga

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/", async (req, res) => {
     users: users,
     color: currentUser.color,    
   });  
-  
+
 });
 
 app.post("/add", async (req, res) => {
@@ -101,6 +101,7 @@ app.post("/user", async (req, res) => {
 });
 
 app.post("/new", async (req, res) => {  
+  
   //https://www.postgresql.org/docs/current/dml-returning.html
   const {name, color} = req.body;
   
