8c8282a87a753eb298327ed6794c17972325e82d
Israel <israeldianga@gmail.com>
2024-10-03 11:17:07 +0300

    Tutapelle

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ app.post("/add", async (req, res) => {
     const data = result.rows[0];    
     const countryCode = data.country_code;
 
-    try {                  
+    try {    
+                    
       await db.query(        
         "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
         [countryCode, currentUserId]
@@ -87,7 +88,7 @@ app.post("/add", async (req, res) => {
   } catch (err) { 
     console.log(err);    
   }    
-   
+
 });
 
 app.post("/user", async (req, res) => {
