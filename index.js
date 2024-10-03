1ef1647723bedc133561ef6d223053b010fa1640
Israel <israeldianga@gmail.com>
2024-10-03 11:18:57 +0300

    Normalcy

--- index.js
+++ index.js
@@ -63,8 +63,7 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
 
-  const input = req.body["country"];
-  
+  const input = req.body["country"];  
   //const currentUser = await getCurrentUser();  
   try {
     const result = await db.query(
