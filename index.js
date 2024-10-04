7032792347b56a612659302eb1a9a9f1ce073e15
Israel <israeldianga@gmail.com>
2024-10-04 13:07:49 +0300

    Empire

--- index.js
+++ index.js
@@ -69,8 +69,7 @@ app.post("/add", async (req, res) => {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]       
-    );
-    
+    );    
 
     const data = result.rows[0];    
     const countryCode = data.country_code;
