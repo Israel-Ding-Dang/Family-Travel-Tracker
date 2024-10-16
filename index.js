b592a02ca3ceef218f88a573655dc69b56a338b2
Israel <israeldianga@gmail.com>
2024-10-16 13:37:11 +0300

    Liitle

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let currentUserId = 2;
 let users = [];
 
 async function checkVisited() {
-  
+
   const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1;", [currentUserId]);
   let countries = [];
 
@@ -38,7 +38,6 @@ async function checkVisited() {
   }); 
 
   return countries; 
-
 }
 
 async function getCurrentUser() {
