bcb3776bdf3ff5bb786550327f9f70018e534f4a
Israel <israeldianga@gmail.com>
2024-10-25 13:12:50 +0300

    Koala

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function checkVisited() {
   }); 
 
   return countries; 
-  
+
 }
 
 async function getCurrentUser() {
@@ -48,6 +48,7 @@ async function getCurrentUser() {
   users = result.rows; 
 
   return users.find((user) => user.id == currentUserId);
+  
 };
 
 app.get("/", async (req, res) => {  
