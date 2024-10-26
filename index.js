2aa04d981240002fb9d80ccf93710d5f307f0c8a
Israel <israeldianga@gmail.com>
2024-10-26 14:58:45 +0300

    Pressure

--- index.js
+++ index.js
@@ -85,9 +85,10 @@ app.post("/add", async (req, res) => {
       console.log(err);      
     }  
   } catch (err) {     
-    console.log(err);    
+    console.log(err); 
+       
   }   
-  
+
 });
 
 app.post("/user", async (req, res) => {
