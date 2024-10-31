479383dfe39c2e41062110b22310266fd22e82aa
Israel <israeldianga@gmail.com>
2024-10-31 14:17:58 +0300

    Plough

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function checkVisited() {
   }); 
 
   return countries; 
-  
+
 }
 
 async function getCurrentUser() {
@@ -47,7 +47,6 @@ async function getCurrentUser() {
   users = result.rows; 
 
   return users.find((user) => user.id == currentUserId);  
-
 };
 
 app.get("/", async (req, res) => {  
