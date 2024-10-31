b1f6b6b5c5125771aeb5198fe2830429d463930d
Israel <israeldianga@gmail.com>
2024-10-31 14:19:15 +0300

    Sprinter

--- index.js
+++ index.js
@@ -61,14 +61,13 @@ app.get("/", async (req, res) => {
     users: users,
     color: currentUser.color, 
   });
-
 });
 
 app.post("/add", async (req, res) => {  
   const input = req.body["country"];    
   //const currentUser = await getCurrentUser();  
   try {
-    
+
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]  
