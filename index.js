a6a3c87d9f1ff43033d3cd6b2628db5f386f5b4d
Israel <israeldianga@gmail.com>
2024-11-05 13:26:14 +0300

    Dance

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ app.post("/add", async (req, res) => {
   const input = req.body["country"];    
   //const currentUser = await getCurrentUser();    
   try {
-
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]  
