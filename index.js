9874855a6cda65bc5929cdb8b6a8ee2e4a2ccbee
Israel <israeldianga@gmail.com>
2024-11-05 13:26:46 +0300

    Alone

--- index.js
+++ index.js
@@ -75,7 +75,6 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
 
     try {  
-
       await db.query(        
         "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
         [countryCode, currentUserId]
