86ff78a3591b0f7c09d544dc7addee36bc67bfb0
Israel <israeldianga@gmail.com>
2024-11-11 16:11:16 +0300

    Systematic

--- index.js
+++ index.js
@@ -116,11 +116,10 @@ app.post("/new", async (req, res) => {
   currentUserId = id;
 
   res.redirect("/");
-  
+
 });
 
 app.listen(port, () => {
-
   console.log(`Server running on http://localhost:${port}`); 
 });
 
