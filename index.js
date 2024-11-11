e9e550ad72366bb0c0d479457bedee01c1b72b1b
Israel <israeldianga@gmail.com>
2024-11-11 14:28:34 +0300

    Starting

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post("/user", async (req, res) => {
 
     res.redirect("/");
   }
-  
+
 });
 
 
@@ -116,6 +116,7 @@ app.post("/new", async (req, res) => {
   currentUserId = id;
 
   res.redirect("/");
+  
 });
 
 app.listen(port, () => {
