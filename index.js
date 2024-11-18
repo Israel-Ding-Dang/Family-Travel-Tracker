efce053032144b1624c95f4db5a31a3a0d98c8ac
Israel <israeldianga@gmail.com>
2024-11-18 16:02:38 +0300

    Potential

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const app = express();
 const port = 3000;
 env.config();
 
-
 const db = new pg.Client({
   user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,    
+    port: process.env.PG_PORT, 
 });
 
 db.connect();
