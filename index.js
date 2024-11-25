f352578ce781e26e4305ff50bfcd05b68d7b44d3
Israel <israeldianga@gmail.com>
2024-11-25 14:20:00 +0300

    Champe

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const db = new pg.Client({
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,     
+    port: process.env.PG_PORT, 
+        
 });
 
 db.connect();
@@ -46,7 +47,7 @@ async function getCurrentUser() {
   users = result.rows; 
 
   return users.find((user) => user.id == currentUserId);
-  
+
 };
 
 app.get("/", async (req, res) => {  
