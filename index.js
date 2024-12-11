9874d5cd82084dae9056d762b0388bb8085f3150
Israel <israeldianga@gmail.com>
2024-12-11 14:19:59 +0300

    Misuse

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post("/add", async (req, res) => {
   } catch (err) { 
     console.log(err);
   }  
-  
+
 });
 
 
@@ -105,6 +105,7 @@ app.post("/user", async (req, res) => {
 
     res.redirect("/");
   }   
+  
 });
 
 
