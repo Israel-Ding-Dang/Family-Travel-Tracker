8b3d6fd9bb891e5f71eed09877b620e5ba2deb92
Israel <israeldianga@gmail.com>
2024-12-11 14:21:24 +0300

    Extension

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ app.post("/add", async (req, res) => {
   } catch (err) { 
     console.log(err);
   }  
-
 });
 
 
@@ -105,7 +104,7 @@ app.post("/user", async (req, res) => {
 
     res.redirect("/");
   }   
-  
+
 });
 
 
