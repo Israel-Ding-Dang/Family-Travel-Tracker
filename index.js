01a1e5e0ae8b6469cf39576d3e18d96c87ae5d87
Israel <israeldianga@gmail.com>
2024-12-16 15:12:00 +0300

    Pointer

--- index.js
+++ index.js
@@ -122,6 +122,7 @@ app.post("/new", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`); 
+  console.log(`Server running on http://localhost:${port}`);
+   
 });
 
