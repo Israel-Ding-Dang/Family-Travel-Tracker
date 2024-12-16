9882b2ab32002dff249e6c06d9ab164c294aef3a
Israel <israeldianga@gmail.com>
2024-12-16 15:12:30 +0300

    Mailord

--- index.js
+++ index.js
@@ -122,7 +122,6 @@ app.post("/new", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-   
+  console.log(`Server running on http://localhost:${port}`);   
 });
 
