5a123eb49cb64a6256819121b7ad786bfb3b2402
Israel <israeldianga@gmail.com>
2025-01-17 14:54:34 +0300

    Money

--- index.js
+++ index.js
@@ -89,7 +89,8 @@ app.post("/add", async (req, res) => {
     }  
   } catch (err) { 
     console.log(err);
-  }       
+  }   
+      
 });
 
 app.post("/user", async (req, res) => {
@@ -101,7 +102,7 @@ app.post("/user", async (req, res) => {
 
     res.redirect("/");
   } 
-  
+
 });
 
 
